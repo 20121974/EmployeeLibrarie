620955d947f73fed69e722b49497a53456308691
20121974 <nfi46102@mail.ru>
2022-11-28 08:00:22 +0300

    1 commit

---
+++ src/main/java/com/skypro/employeelibrarie/EmployeeLibrarieApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employeelibrarie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLibrarieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLibrarieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/employeelibrarie/EmployeeLibrarieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employeelibrarie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLibrarieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
